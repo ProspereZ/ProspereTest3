b0564e889cfc726b7b301f1b1391ba64e846f953
ProspereN <ProspereN@PC215-14>
2021-10-21 14:58:17 +0200

    Add project files.

---
+++ ProspereTest3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProspereTest3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=20; i++)
+            {
+                Console.WriteLine(i);
+                if(i % 2 ==0)
+                {
+                    Console.WriteLine(0);
+                }
+            }
+              
+        }
+    }
+}
